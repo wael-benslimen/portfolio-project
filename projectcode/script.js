f0a75688e278bdfa8c79193c6c99afb8a3d36daf
wael-benslimen <waelbenslimen1111@gmail.com>
2025-01-02 12:52:12 +0100

    animations

--- projectcode/script.js
+++ projectcode/script.js
@@ -59,3 +59,24 @@ function expand(id) {
     document.querySelector(id).style.padding = '1rem'
     element.style.transition = "all 0.3s ease";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // determine if objects are within the viewport
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+                return;
+            }
+            entry.target.classList.remove('in-view');
+        });
+    });
+
+    // Get all the elements with the .animate class applied
+    const allAnimatedElements = document.querySelectorAll('.animate');
+
+    // Add the observer to each of those elements
+    allAnimatedElements.forEach((element) => observer.observe(element));
+
+}); 
