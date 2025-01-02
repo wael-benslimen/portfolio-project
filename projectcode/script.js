6e05a259270bffefe8311aec91c9e803f753f108
wael-benslimen <waelbenslimen1111@gmail.com>
2025-01-02 10:20:47 +0100

    adds to the skills page

--- projectcode/script.js
+++ projectcode/script.js
@@ -1,50 +1,50 @@
 console.log("page loaded");
 
 
-function over(element){
+function over(element) {
     element.style.transform = "scale(1.06)";
     element.style.boxShadow = "0 0 30px rgba(251, 191, 36, 0.5)";
     element.style.transition = "all 0.2s ease";
 }
 
-function out(element){
-    element.style.transform ="scale(1)";
+function out(element) {
+    element.style.transform = "scale(1)";
     element.style.boxShadow = "none";
 }
 
 
-function buttonOver(element){
+function buttonOver(element) {
     element.style.backgroundColor = "#fbbf24";
     element.style.color = "#1f2937";
     element.style.transform = "transLateY(-2px)";
     element.style.transition = "all 0.2s ease";
 }
 
-function buttonOut(element){
+function buttonOut(element) {
     element.style.backgroundColor = "transparent";
     element.style.color = "white";
     element.style.transform = "transLateY(0)";
 }
 
-function buttonIn(element){
+function buttonIn(element) {
     element.style.transform = "transLateY(-2px)";
     element.style.transition = "all 0.3s ease";
 }
 
-function buttonOute(element){
+function buttonOute(element) {
     element.style.transform = "transLateY(0)";
-    
+
 }
 
-function navOver(id){
+function navOver(id) {
     document.getElementById(id).style.color = "#f59e0b";
     document.getElementById(id).style.transition = "all 0.3s ease";
 }
-function navOut(id){
+function navOut(id) {
     document.getElementById(id).style.color = "#e2e8f0";
 }
 
-function downloadFile(){
+function downloadFile() {
     var link = document.createElement('a');
     link.href = './assets/images/751826dddbf2011014c758a865c7df6b.jpg';
     link.download = 'Abdallah-Yessine-Kriaa-CV.jpg';
@@ -52,4 +52,10 @@ function downloadFile(){
     link.click();
     document.body.removeChild(link);
 
+}
+
+function expand(id) {
+    document.querySelector(id).style.height = '70%'
+    document.querySelector(id).style.padding = '1rem'
+    element.style.transition = "all 0.3s ease";
 }
